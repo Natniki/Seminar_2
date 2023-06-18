8b80106eff1fae5155fe2672af087e463872192e
Natallia Nikitsina <natiknik73@gmail.com>
2023-06-18 12:07:59 +0300

    added task_1

---
+++ task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int fortrest(int i)
+{
+    Console.Write(i + " => ");
+    int res = i /10 % 10;
+    return res;
+}
+Console.Write("input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num>999 || num<100)
+{
+    Console.WriteLine(" oshibka: chislo ne trehznachnoe!");
+}   
+else
+{ 
+    int result = fortrest( num);
+    Console.WriteLine(result);
+}
+    
+        
+    
+
+
+
+
+
